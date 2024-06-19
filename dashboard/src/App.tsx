448ee9b59c6c7e7d59889dd4c268c23b394230d4
angmcruz <108002776+angmcruz@users.noreply.github.com>
2024-06-19 13:57:49 -0500

    basictable

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,21 +1,16 @@
 
 import './App.css'
 import Grid from '@mui/material/Unstable_Grid2';
+import BasicTable from './components/BasicTable';
 
 
 function App() {
-
-
 	return (
-      <Grid container spacing={5}>
-          <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-          <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-          <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-          <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-          <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-          <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
-        </Grid>
-    )
+    <Grid xs={12} md={6} lg={9} >
+	       <BasicTable />
+	    </Grid>     
+	
+	)
 }
 
 
