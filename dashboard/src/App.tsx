a6ca77f1ba6d0bdf0e696645acf3b821cb3f6036
angmcruz <108002776+angmcruz@users.noreply.github.com>
2024-06-26 14:57:58 -0500

    upd
    NO SABMOS PORQUE HAY LINEAS ROJAS

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -6,20 +6,120 @@ import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import WeatherChart from './components/WeatherChart';
 import ControlPanel from './components/ControlPane';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+	let [Indicators, setIndicators] = useState([])
+	let [rowsTable, setRowsTable] = useState([])
+
+	useEffect( ()=>{
+			//autoejecuta
+		(async ()=>{ 
+
+			{/*
+
+			let API_KEY = "c33c7a84485848efbc31c87efe17e9ef"
+			let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+			let savedTextXML = await response.text();
+			*/}
+
+			{/* /* USAR LOCALSTORAGE REORGANIZAR CODIGO}*/}
+			let savedTextXML = localStorage.getItem("openWeatherMap")
+            let expiringTime = localStorage.getItem("expiringTime")
+			let nowTime = (new Date()).getTime();
+
+
+			if(expiringTime === null || nowTime > parseInt(expiringTime)) {
+
+				{/* 5. Request */}
+
+				let API_KEY = "c33c7a84485848efbc31c87efe17e9ef"
+				let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+				savedTextXML = await response.text();
+
+
+				{/* 6. Diferencia de tiempo */}
+
+				let hours = 1
+				let delay = hours * 3600000
+
+
+				{/* 7. En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */}
+
+				localStorage.setItem("openWeatherMap", savedTextXML)
+				localStorage.setItem("expiringTime", (nowTime + delay ).toString() )
+			}
+
+
+			//xml parser (analizador)
+			const parser = new DOMParser();
+            const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+			let dataToIndicators = new Array()
+
+			// contenido el xml en el arreglo 
+			let location = xml.getElementsByTagName("location")[1]
+
+			let geobaseid = location.getAttribute("geobaseid")
+			dataToIndicators.push(["Location","geobaseid", geobaseid])
+
+			let latitude = location.getAttribute("latitude")
+			dataToIndicators.push(["Location","Latitude", latitude])
+
+			let longitude = location.getAttribute("longitude")
+			dataToIndicators.push(["Location","Longitude", longitude])
+
+			console.log( dataToIndicators )
+
+			//renderizado array
+			let indicatorsElements = Array.from(dataToIndicators).map(
+				(element) => <Indicator title={element[0]} subtitle={element[1]} value={element[2]} />
+			)
+			
+			//se modifica el estado de la variable por la funcion
+			setIndicators(indicatorsElements)
+
+			let arrayObjects = Array.from( xml.getElementsByTagName("time") ).map( (timeElement) =>  {
+					
+				let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
+
+				let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " "+  timeElement.getElementsByTagName("windDirection")[0].getAttribute("code") 
+				   
+				return { "rangeHours": rangeHours,"windDirection": windDirection }
+			   
+			})
+
+			arrayObjects = arrayObjects.slice(0,8)
+
+			setRowsTable(arrayObjects)
+
+
+
+		 })()
+
+
+
+
+	}, [] )
+
+
 	return (
     <>   
 
     <Grid container spacing={5}>
       <Grid xs={12} md={4} lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+		
+	  	{Indicators[0]}
+	        {/*<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
 	    </Grid>
       <Grid xs={12} md={4} lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+	  {Indicators[1]}
+	        {/*<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />*/}
 	    </Grid>
       <Grid xs={12} md={4} lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+	  {Indicators[2]}
+	        {/*<Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />*/}
 	    </Grid>
       <Grid xs={12} md={4} lg={2}>
 	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
@@ -34,7 +134,7 @@ function App() {
 	        <Summary></Summary>
 	    </Grid> 
       <Grid xs={12} md={6} lg={9} >
-	       <BasicTable />
+	  	<BasicTable rows={rowsTable}></BasicTable>
 	    </Grid>  
 		<Grid xs={12} lg={2}>
              <ControlPanel />
--- dashboard/src/components/BasicTable.tsx
+++ dashboard/src/components/BasicTable.tsx
@@ -6,52 +6,64 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+// function createData(
+//   name: string,
+//   calories: number,
+//   fat: number,
+//   carbs: number,
+//   protein: number,
+// ) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+
+interface Config {
+  rows: Array<object>;
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+export default function BasicTable(data:Config) {
+  let [rows, setRows] = useState([])
 
-export default function DenseTable() {
+  useEffect( () => {
+
+    (()=> {
+
+        setRows(data.rows)
+
+    })()
+
+}, [data] )
+
+
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell>Rango de horas</TableCell>
+          <TableCell align="right">Dirección del viento</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
+            key={row.rangeHours}  
+        >
+            <TableCell component="th" scope="row">
+                {row.rangeHours}
+            </TableCell>
+            <TableCell align="right">{row.windDirection}</TableCell>
+        </TableRow>
           ))}
         </TableBody>
       </Table>
